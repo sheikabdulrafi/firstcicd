0a3c2a72ae1e91d6b7886835cd33bb6c777db61b
sheikabdulrafi <sheikabdulrafi@gmail.com>
2024-06-27 12:11:49 +0530

    added test case

--- src/test/java/com/devops/dev/DevApplicationTests.java
+++ src/test/java/com/devops/dev/DevApplicationTests.java
@@ -1,5 +1,7 @@
 package com.devops.dev;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class DevApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(true, true);
 	}
 
 }
